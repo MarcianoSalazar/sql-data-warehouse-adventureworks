1ab0dcf6ab332936e06c5cfe742f8e8be8ec774a
Marciano Salazar <168376936+Sheyxii@users.noreply.github.com>
2026-07-16 16:55:51 +0800

    Create dim_customers_check.sql

---
+++ tests/gold_check/dim_customers_check.sql
@@ -0,0 +1,16 @@
+-- Check Consistency 
+SELECT customer_key, customer_id, name, customer_type
+FROM gold.dim_customers
+WHERE (customer_type = 'Store' AND name IS NULL)
+   OR (customer_type = 'Individual' AND name IS NULL);
+
+-- Check Duplicates
+SELECT customer_id, COUNT(*) 
+FROM gold.dim_customers
+GROUP BY customer_id
+HAVING COUNT(*) > 1;
+
+-- Check breakdown of Number of Customer type 
+SELECT customer_type, COUNT(*) AS total
+FROM gold.dim_customers
+GROUP BY customer_type;
